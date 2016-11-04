8094cec469b1f94d270cc1506bdd9f4d05b952a7
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-11-04 20:34:48 +0000

    Added server tests for get / and get /1234 unixtime.

--- tests/server.js
+++ tests/server.js
@@ -20,13 +20,45 @@ describe(wrongPath, () => {
     it("Should get null data from server", (done) => {
         chai.request(server).get(wrongPath)
         .end((err, res) => {
-            expect(res.unixtime).to.equal(null);
-            expect(res.natural).to.equal(null);
+            expect(err).to.be.null;
+            expect(res).to.be.json;
+            //console.log(JSON.stringify(res));
+            var json = JSON.parse(res.text);
+            expect(json.unix).to.equal(null);
+            expect(json.natural).to.equal(null);
+            done();
+        });
+    });
+});
+
+
+describe("/1234 unixtime", () => {
+    it("Should get unixtime and date from server", (done) => {
+        chai.request(server).get("/1234")
+        .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.be.json;
+            
+            var json = JSON.parse(res.text);
+            expect(json.unix).to.equal(1234);
+            expect(json.natural).to.match(/1970/);
+            done();
+        });
+    });
+});
+
+
+describe("/ GET", () => {
+    it("Should get the index.html from server", (done) => {
+        chai.request(server).get('/')
+        .end((err, res) => {
+            expect(err).to.be.null;
+            //console.log(JSON.stringify(res));
+            //expect(res.unixtime).to.equal(null);
+            //expect(res.natural).to.equal(null);
+            expect(res).to.be.html;
             done();
             
-        })
-    }
-    )
-    
-}
-);
+        });
+    });
+});
